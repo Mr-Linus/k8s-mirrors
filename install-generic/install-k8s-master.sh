5532b444337b23d278e35f9c64495b5f397ed340
Funky <708863861@qq.com>
2019-03-04 14:14:04 +0800

    add install shell

--- install-generic/install-k8s-master.sh
+++ install-generic/install-k8s-master.sh
@@ -8,7 +8,7 @@ EOF
 
 ## init k8s 
 kubeadm init --kubernetes-version=v1.13.4  --pod-network-cidr=10.244.0.0/16 --config $(pwd)/kubeadm-images.yaml
-## --kubernetes-version=v1.13.3 指定版本
+## --kubernetes-version=v1.13.4 指定版本
 ## 如果你使用calico或者flannel网络,必须加上参数--pod-network-cidr=10.244.0.0/16
 ## 构建 k8s 配置文件夹并拷贝管理员默认配置文件
 mkdir -p $HOME/.kube
