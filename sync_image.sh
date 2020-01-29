cd6be88753ce7585824c502cd22cb0c4215e7a0f
mr-linus <mrfunky001@gmail.com>
2020-01-29 15:02:57 +0800

    update sync function

--- sync_image.sh
+++ sync_image.sh
@@ -1,7 +1,6 @@
 echo "Pulling K8S Images"
 version=$(kubeadm config images list | head -1 | awk -F: '{ print $2 }')
 sudo docker run --rm -it \
-        -v $(pwd)/image.txt:/image-pull/image.txt \
         -v /var/run/docker.sock:/var/run/docker.sock  \
         registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:k8s-$version
 
